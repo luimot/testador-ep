b5957e492b6451dcbcdd80e687bad52614d5cf78
Luiz Mota <luiz.mota1999@usp.br>
2020-08-31 12:11:20 -0300

    adicionadas variáveis

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ Esse script bash utiliza ep1.c como arquivo principal (pode alterar em `run.sh` 
 Para utiliza-lo abra o terminal e digite:
 
 		$ sudo chmod 755 run.sh
+Altere as variáveis iniciais do código fonte para usá-las no seu caso apropriado
 
 E depois,
 
 		$ ./run.sh <Número do caso>
-`run.sh` toma como argumento somente o caso, se for Estrela de Koch, H Tree ou o fractal 
+`run.sh` toma como argumento somente o caso, se for Estrela de Koch, H Tree ou o fractal
+ 
 Pronto!
--- run.sh
+++ run.sh
@@ -1,14 +1,26 @@
 #! /bin/bash
+###Altere os valores NOMEARQ para a saída (.ps) que você programou no seu programa .c
+###Altere o valor de INICIO e FIM para alterar até que ordem você quer que o script execute
+NOMEARQ="ep1out"
+INICIO=6
+FIM=6
 
+[[ ! -f tt.in ]] && touch tt.in
+[[ ! -f $NOMEARQ".pdf" ]] && touch $NOMEARQ".pdf"
 if gcc -Wall -std=c99 -pedantic -O2 ep1.c -o ep1.out -lm ; then
+	if [ $# -eq 0 ]
+	then
+		echo "Nao foram encontrados argumentos, coloque-os para selecionar o tipo de fractal!"
+	else
 	#Testa de 0 a 9:
-	for NUM_TEST in {0..9}
-	do
-		printf "${1}\n${NUM_TEST}" > tt.in
-		./ep1.out < tt.in
-		ps2pdf ep1out.ps && google-chrome ep1out.pdf &
-		sleep 2
-	done
+		for (( NUM_TEST=$INICIO; NUM_TEST<=$FIM; NUM_TEST++ ))
+		do
+			printf "${1}\n${NUM_TEST}" > tt.in
+			./ep1.out < tt.in
+			ps2pdf $NOMEARQ".ps" && google-chrome $NOMEARQ".pdf" &
+			sleep 2
+		done
+	fi
 else
-	echo "Erro!"
+	echo "Erro ou Warning!"
 fi
